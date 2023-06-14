e83ab4bd379db690f7e5303446e02a07ad63a30b
satya5570 <satyanarayanasatapathy65@gmail.com>
2023-06-15 00:33:16 +0530

    Updates

---
+++ precache-manifest.f17355cee0f45b3f982c561ce48aece5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a209d294eaca23ddd97",
+    "url": "/textutils/static/css/main.2594c8d8.chunk.css"
+  },
+  {
+    "revision": "4a209d294eaca23ddd97",
+    "url": "/textutils/static/js/main.4a209d29.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textutils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "91449b700f0ab8a2a413",
+    "url": "/textutils/static/js/runtime~main.91449b70.js"
+  },
+  {
+    "revision": "39f089c621887aa46cf2d20e62cd5fe2",
+    "url": "/textutils/index.html"
+  }
+];
